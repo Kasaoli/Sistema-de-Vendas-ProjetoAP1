bd542f3b90c9cf2269bf63f63eb40b7adce4822e
Kauan Santos <kauansantos60@gmail.com>
2022-05-06 11:40:32 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+op = 0
+while True:
+  print("########################")
+  print("#    BEM VINDO         #")
+  print("#                      #")
+  print("#    1 - ENTRAR        #")
+  print("#    2 -  SAIR         #")
+  print("#                      #")
+  print("########################")
+  
+  op = int(input('Digite uma opção: '))
+  if op > 2 or op < 0:
+    print("Opção inválida.")
