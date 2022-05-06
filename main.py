25e4d6aaa0aecd9300bf2064a40439e55f6892d8
Kasaoli <kauansantos60@gmail.com>
2022-05-06 12:42:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 op = 0
 while True:
-  print("########################")
-  print("#     BEM VINDO        #")
-  print("#                      #")
-  print("#    1 - ENTRAR        #")
-  print("#    2 -  SAIR         #")
-  print("#                      #")
-  print("########################")
-  
-  op = int(input('Digite uma opção: '))
-  if op == 1:
-    print()
-  elif op == 2:
-    quit()
-  elif op > 2 or op < 0:
-    print("Opção inválida.")
+    print("########################")
+    print("#     BEM VINDO        #")
+    print("#                      #")
+    print("#    1 - ENTRAR        #")
+    print("#    2 -  SAIR         #")
+    print("#                      #")
+    print("########################")
+
+    op = int(input('Digite uma opção: '))
+    if op == 1:
+        print()
+    elif op == 2:
+        quit()
+    else:
+        print("Opção inválida.")
